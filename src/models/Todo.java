5ddf4581f21a742c2795defde748c6861ddfc2f4
roa.ama <karikarihahaha@yahoo.co.jp>
2022-06-10 14:38:49 +0900

    データベース関連のファイルを追加

---
+++ src/models/Todo.java
@@ -0,0 +1,64 @@
+package models;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todol")
+
+public class Todo {
+
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "todo", length = 255, nullable = false)
+    private String todo;
+
+    @Column(name = "report_date", nullable = false)
+    private LocalDate reportDate;
+
+    @Column(name = "delete_flag", nullable = false)
+    private Integer deleteFlag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    public LocalDate getReportDate() {
+        return reportDate;
+    }
+
+    public void setReportDate(LocalDate reportDate) {
+        this.reportDate = reportDate;
+    }
+
+    public Integer getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Integer deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+
+}
